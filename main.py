2f249d84d91e8e93074cf3b11104d5fe0c09352c
Donggeon Kim <128569780+ehdrjs13@users.noreply.github.com>
2026-04-06 18:31:01 +0900

    implement main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
+import time
 
-#남은 할 일,, wildcard 알고리즘 for user and computer, 
+#남은 할 일,, wildcard 알고리즘 for user and computer, when deck is empty.. 
+# 디버그하기, 
 
 class Player:
     def __init__(self) -> None:
@@ -13,12 +15,12 @@ class Player:
 
         pass
 
-    def checkIdx(self,reference:tuple) -> int | bool:
+    def checkIdx(self,reference:tuple) -> int:
         for idx in range(len(self.hand)):
             if self.hand[idx][0] == reference[0] or self.hand[idx][1] == reference[1] or self.hand[idx][1] == 11:
                 return idx
         
-        return False
+        return 1000
     
 class Game:
     def __init__(self):
@@ -98,9 +100,11 @@ def make_initial_card(deck:list,player:Player):
 
 
 def show_game_state(game:Game,player:Player) -> None:
+    print("")
     print(f"Top Card: {game.top}")
     print(f"Deck: {player.hand}")
     print(f"Number of cards in deck: {len(game.Deck)}")
+    print("")
     
     return (game.top,len(game.Deck),player.hand)
 
@@ -115,10 +119,10 @@ def is_playable_card(card:tuple,reference:tuple):
 def is_valid_card_index():
     return
 
-def play_user_turn():
+#main에 있는 알고리즘 복붙하기
+def play_user_turn(game:Game,user:Player):
     return
-
-def play_computer_turn():
+def play_computer_turn(game:Game,computer:Player):
     return
 
 def win(result:int) -> None:
@@ -147,29 +151,32 @@ def main():
     make_initial_card(game.Deck,computer)
     game.makeTopCard()
     print("Welcome to One card Game! Select Game Mode. (Play Mode:0 Debug Mode:1)")
-    game.mode = input("Enter GameMode:") == 1
+    game.mode = int(input("Enter GameMode:")) == 1
 
     while True:
-        
+        print(f"game mode is {game.mode}")
+    
         #User 차례일 때
         if game.turn:
             print("User playing. ")
-            display_status("User",user.hand,game.top)
+            show_game_state(game,user)
             if len(user.hand) == 0:
                 win(0)
                 break
-            if user.checkIdx(game.top) == False:
+            if user.checkIdx(game.top) == 1000:
                 user.hand.append(game.draw())
                 print("")
-                if user.checkIdx(game.top) != False:
+                if user.checkIdx(game.top) != 1000:
+                    print(user.checkIdx(game.top))
                     game.top = user.hand.pop(int(user.checkIdx(game.top)))
-                    game.turn = not game.turn
-                    print("")
+                    
+                    print("유저가 자동으로 뽑은 카드 내기")
                 game.turn = not game.turn
-                print("")
+            
+            
             
             else:   
-                command = input(f"Enter card index (0–{len(user.hand)}) or q (quit):")
+                command = int(input(f"Enter card index (0–{len(user.hand)}) or q (quit):"))
                 if command == "q":
                     quit()
                 if not is_playable_card(user.hand[int(command)],game.top):
@@ -183,38 +190,51 @@ def main():
                     else:
                         game.top = user.hand.pop(int(command))
                         game.turn = not game.turn
+            print("")
+
 
         #Computer 차례일 때
         elif not game.turn:
             print("Computer Playing. ")
-            ## Debug mode인 경우 제어문
+            ## Debug mode인 경우
             if game.mode:
+                print("computer status")
                 show_game_state(game,computer)
+            # 덱에 카드가 없으면 이긴다. 
             if len(computer.hand) == 0:
                 win(2)
                 break
-            if user.checkIdx(game.top) == False:
-                user.hand.append(game.draw())
+            #낼 카드가 없으면
+            if computer.checkIdx(game.top) == 1000:
+                computer.hand.append(game.draw()) #덱에서 하나 뽑고
+                show_game_state(game,computer)
                 print("No card playable, Getting one card from the Deck. ")
-                if user.checkIdx(game.top) != False:
-                    game.top = user.hand.pop(int(user.checkIdx(game.top)))
-                    game.turn = not game.turn
-                    print("The card is automatically Played. ")
-                game.turn = not game.turn
-                print("자동으로 순서 넘어감")
-
-            else:
-                command = computer.checkIdx(game.top)
-                if command == False:
-                    print("lose") # 이 부분 추가 필요
+                if computer.checkIdx(game.top) != 1000: #다시 체크했을 때 낼 카드가 있으면
+                    print(computer.checkIdx(game.top))
+                    game.top = computer.hand.pop(int(computer.checkIdx(game.top))) #그걸 자동으로 낸다. 
                     
+                    print("The card is automatically Played. ")
+                game.turn = not game.turn # 차례 바꾸기
+                
+                    
+                print("자동으로 순서 넘어감\n")
+
+            else: #낼 카드가 있다면
+                command = computer.checkIdx(game.top) #컴퓨터가 알아서 골라서 낸다. 
+
                 if computer.hand[command][1] == 10:
                     game.top = computer.hand.pop(int(command))
-                elif computer.hand[command][1] == 11:
+                if computer.hand[command][1] == 11: #낸 카드가 wildcard면
                     game.wild()
                 else:
                     game.top = computer.hand.pop(int(command))
                     game.turn = not game.turn
+                
+            print("")
+
+
+    
+
 
 
 
