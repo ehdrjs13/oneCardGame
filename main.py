83a0172016fb5944244eb90005ff077f39fd1e1c
Donggeon Kim <128569780+ehdrjs13@users.noreply.github.com>
2026-04-07 16:24:15 +0900

    add annotations

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import random
 
 class Player:
     def __init__(self) -> None:
-        self.hand = []
+        self.hand = [] #유저가 가지고 있는 카드
         
         pass
     
+    #유저가 가지고 있는 카드의 모음에 새로운 카드를 추가한다. 
     def getCard(self,card:tuple) -> None:
         self.hand.append(card)
 
@@ -13,10 +14,11 @@ class Player:
 
     def checkIdx(self,reference:tuple) -> int:
         for idx in range(len(self.hand)):
-            if self.hand[idx][0] == reference[0] or self.hand[idx][1] == reference[1] or self.hand[idx][1] == 11:
+            #value, suit가 같거나 낸 카드가 WildCard라면
+            if self.hand[idx][0] == reference[0] or self.hand[idx][1] == reference[1] or self.hand[idx][1] == 11: 
                 return idx
         
-        return 1000
+        return -1
     
 class Game:
     def __init__(self):
@@ -26,12 +28,14 @@ class Game:
         self.turn = True #User 차례가 True
 
         pass
-
+    
+    #받은 덱(리스트)를 게임의 덱으로 사용한다. 
     def updateDeck(self,deck):
         self.Deck = deck
 
         pass
-
+    
+    #일반 카드가 뽑힐 때 까지 Top card 뽑기
     def makeTopCard(self) -> tuple:
         while True:
             top = self.Deck.pop()
@@ -40,9 +44,11 @@ class Game:
                 return
             
     def wild(self,isBot:bool,player:Player):
+        #컴퓨터용 wildCard 절차 진입
         if isBot: 
             suit = self.autoChoice(player)
         
+        #유저인 경우 유저가 직접 카드 고르기. 
         else: 
             while True:
                 suit = input("Which Suit Do you Choose? 0:♡ 1:♠ 2:◇ 3:3")
@@ -59,7 +65,7 @@ class Game:
         
         return
     
-    @staticmethod
+    @staticmethod #컴퓨터가 WildCard를 사용했을 때 자동으로 suit를 고르는 알고리즘. 
     def autoChoice(player:Player) -> int:
         suits = [card[0] for card in player.hand]
         count = [0,0,0,0]
@@ -85,7 +91,7 @@ class Game:
 
                 
 
-
+#초기 덱 만들어서 리스트로 반환
 def make_deck():
     deck = []
     for suit in range(4):
@@ -96,13 +102,14 @@ def make_deck():
     
     return deck
 
+#인자로 받은 Player에게 카드를 6장씩 덱에서 빼서 나눠준다. 
 def make_initial_card(deck:list,player:Player):
     for i in range(6):
         player.getCard(deck.pop())
 
     return None
 
-
+#게임의 상태를 보여준다. 
 def show_game_state(game:Game) -> None:
     print("")
     print(f"Top Card: {suitFormater(game.top)}")
@@ -113,6 +120,7 @@ def show_game_state(game:Game) -> None:
     return 
 
 def suitFormater(card:tuple) -> str:
+    #카드 데이터(튜플)을 사람이 알아보기 쉬운 형식의 데이터로 바꿔서 보여준다. 
     if card[1] == 10:
         value = "S"
     elif card[1] ==11:
@@ -131,7 +139,7 @@ def suitFormater(card:tuple) -> str:
     return 
 
 def show_player_hand(player:Player):
-
+    #카드 데이터를 포매팅해서 보여준다. 
     for i in player.hand:
         print(f"{player.hand.index(i)}: {suitFormater(i)}")
         
@@ -156,12 +164,11 @@ def play_user_turn(game:Game,user:Player):
     if len(user.hand) == 0:
         win(0)
         quit()
-    if user.checkIdx(game.top) == 1000:
+    if user.checkIdx(game.top) == -1: #낼 카드가 없다면
         user.hand.append(game.draw())
         print("")
-        if user.checkIdx(game.top) != 1000:
-            print(user.checkIdx(game.top))
-            game.top = user.hand.pop(int(user.checkIdx(game.top)))
+        if user.checkIdx(game.top) != -1: #뽑은 카드를 낼 수 있다면
+            game.top = user.hand.pop(int(user.checkIdx(game.top))) #그걸 낸다. 
             
             print("유저가 자동으로 뽑은 카드 내기")
         game.turn = not game.turn
@@ -174,28 +181,28 @@ def play_user_turn(game:Game,user:Player):
         if command == "q":
             quit()
         
-        if not command.isdigit():
+        if not command.isdigit(): 
             print("Invalid input...")
             return
 
         command = int(command)
 
-        if not is_valid_card_index(user,command):
+        if not is_valid_card_index(user,command): #덱에 없는 카드 인덱스를 선택하면
             print("Not Valid Index. Try again. ")
             return
 
-        if not is_playable_card(user.hand[int(command)],game.top):
-            print("이카드안됨")
+        if not is_playable_card(user.hand[int(command)],game.top): #낼 수 없는 카드를 선택하면
+            print("This card is not playable.")
         
         else:
-            if user.hand[command][1] == 10:
+            if user.hand[command][1] == 10: #S카드이면 내기만 하고 순서는 안 바꾸기
                 game.top = user.hand.pop(int(command))
-            elif user.hand[command][1] == 11:
+            elif user.hand[command][1] == 11: #W 카드이면 W카드 절차 밟고 순서 바꾸기
                 user.hand.pop(int(command))
                 game.wild(False,user)
                 game.turn = not game.turn
             else:
-                game.top = user.hand.pop(int(command))
+                game.top = user.hand.pop(int(command)) #일반 카드이면 내고 순서 바꾸기
                 game.turn = not game.turn
     print("")
     return
@@ -211,12 +218,12 @@ def play_computer_turn(game:Game,computer:Player) -> None:
         win(2)
         quit()
     #낼 카드가 없으면
-    if computer.checkIdx(game.top) == 1000:
+    if computer.checkIdx(game.top) == -1:
         computer.hand.append(game.draw()) #덱에서 하나 뽑고
         show_game_state(game)
         show_player_hand(computer)
         print("No card playable, Getting one card from the Deck. ")
-        if computer.checkIdx(game.top) != 1000: #다시 체크했을 때 낼 카드가 있으면
+        if computer.checkIdx(game.top) != -1: #다시 체크했을 때 낼 카드가 있으면
             print(computer.checkIdx(game.top))
             game.top = computer.hand.pop(int(computer.checkIdx(game.top))) #그걸 자동으로 낸다. 
             
@@ -229,10 +236,11 @@ def play_computer_turn(game:Game,computer:Player) -> None:
     else: #낼 카드가 있다면
         command = computer.checkIdx(game.top) #컴퓨터가 알아서 골라서 낸다. 
 
-        if computer.hand[command][1] == 10:
-            game.top = computer.hand.pop(int(command))
+        
+        if computer.hand[command][1] == 10: #낸 카드가 s카드라면
+            game.top = computer.hand.pop(int(command)) #내기만 하고 순서는 안 바꾼다. 
         if computer.hand[command][1] == 11: #낸 카드가 wildcard면
-            computer.hand.pop(int(command))
+            computer.hand.pop(int(command)) 
             game.wild(True,computer)
             game.turn = not game.turn
         else:
@@ -242,6 +250,7 @@ def play_computer_turn(game:Game,computer:Player) -> None:
     print("")
     return
 
+#0이면 유저 승, 1이면 컴퓨터 승, 2이면 무승부. 
 def win(result:int) -> None:
     if result == 0:
         display_winner("User")
@@ -257,7 +266,7 @@ def win(result:int) -> None:
         quit()
         pass
     
-
+#승자를 축하하는 문구를 띄우는 함수
 def display_winner(name:str) -> None:
 
     trophy_art = r"""
@@ -282,16 +291,17 @@ def display_winner(name:str) -> None:
 
     return
 
+#덱에 카드가 더 이상 없을 떄 진행하는 절차, 유저 카드 개수와 컴퓨터 카드 개수를 비교하여 승자를 가린다. 
 def noCardInDeck(user:Player,computer:Player) -> None:
-    userCard = len(user.hand)
-    computerCard = len(computer.hand)
+    userCard = len(user.hand) #유저 카드 개수
+    computerCard = len(computer.hand) #컴퓨터 카드 개수
 
     if userCard > computerCard:
-        win(0)
+        win(0) #유저가 많으면 유저 승
     elif userCard < computerCard:
-        win(1)
-    else:
-        win(2)
+        win(1) #컴퓨터가 많으면 컴퓨터 승
+    else: #두 플레이어 카드 개수가 같다면
+        win(2) #무승부
     
     return
     
@@ -300,10 +310,10 @@ def main() -> None:
     user = Player()
     computer = Player()
     game = Game()
-    game.updateDeck(make_deck())
-    make_initial_card(game.Deck,user)
-    make_initial_card(game.Deck,computer)
-    game.makeTopCard()
+    game.updateDeck(make_deck()) #덱 만들기
+    make_initial_card(game.Deck,user) #유저 6장 뽑기. 
+    make_initial_card(game.Deck,computer) #컴퓨터 6장 뽑기
+    game.makeTopCard() #Top Card 만들기
     print("Welcome to One card Game! Select Game Mode. (Play Mode:0 Debug Mode:1)")
     while True:
         modeIdx = input("Enter GameMode:")
@@ -314,6 +324,7 @@ def main() -> None:
             break
 
     while True:
+        #덱이 비었으면 대결 상대끼리 남은 카드 개수 비교하는 
         if len(game.Deck) == 0:
             noCardInDeck(user,computer)
             
